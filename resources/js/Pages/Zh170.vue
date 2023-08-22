3ada3f8905701d065a3d342464d661507de79ba7
Rodrigo Blanco <rodrigob_conexiondigital@hotmail.com>
2023-08-22 15:27:23 -0300

    WIP

--- resources/js/Pages/Zh170.vue
+++ resources/js/Pages/Zh170.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">Billing record & AR satus report!</h1>
+            <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">Billing record & AR status report!</h1>
                 <div class="flex gap-4 p-4 sm:p-8 bg-white shadow sm:rounded-lg"> 
                     <div class="md:w-1/3 align-middle">
                         <label for="exampleFormControlInput1" class="form-label">Posting date</label> 
                     </div>
                     <div class="md:w-2/3">
-                        <VueDatePicker v-model="date" range multi-calendars />
+                        <VueDatePicker v-model="date" range multi-calendars @closed="alertFn"/>
                     </div>
                 </div> 
         </div>
@@ -26,4 +26,7 @@ onMounted(() => {
   const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
   date.value = [startDate, endDate];
 })
+const alertFn = () => {
+  console.log(date.value[0]+'to'+date.value[1]);
+}
 </script>
