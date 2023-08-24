041dd6c574f7e00b517e5d10c960784f6b40fe06
Rodrigo Blanco <rodrigob_conexiondigital@hotmail.com>
2023-08-24 19:44:30 -0300

    WIP

--- app/Http/Controllers/ApiTestController.php
+++ app/Http/Controllers/ApiTestController.php
@@ -12,14 +12,14 @@ class ApiTestController extends Controller
 {
     public function index(Request $request)
     {
-        $response = Http::withHeaders([
-            'X-RapidAPI-Key' => 'SIGN-UP-FOR-KEY',
-            'X-RapidAPI-Host' => 'geography4.p.rapidapi.com'
-        ])->post('http://gd2osapp02.usiglobal.com:8000/sap/fi_reports', [
-            'sortBy' => 'name',
-            'limit' => '10',
-            'sortOrder' => 'asc',
-        ]);
+        // $response = Http::withHeaders([
+        //     'X-RapidAPI-Key' => 'SIGN-UP-FOR-KEY',
+        //     'X-RapidAPI-Host' => 'geography4.p.rapidapi.com'
+        // ])->post('http://gd2osapp02.usiglobal.com:8000/sap/fi_reports', [
+        //     'sortBy' => 'name',
+        //     'limit' => '10',
+        //     'sortOrder' => 'asc',
+        // ]);
         // $response = Http::post(
         //     'http://gd2osapp02.usiglobal.com:8000/sap/fi_reports',
         //     // array(
@@ -28,7 +28,7 @@ class ApiTestController extends Controller
         //     //     'password' => 'testpassword'
         //     // )
         // );
-        var_dump($response?$response:'');
+        // var_dump($response?$response:'');
         // var_dump($request->input('requestData'));
         return Inertia::render('Test', [
             // 'data'=> $response,
