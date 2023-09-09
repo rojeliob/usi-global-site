f783ee84c8f3c3ea8c22b81d6f3cb525fa7f5439
Rodrigo Blanco <rodrigob_conexiondigital@hotmail.com>
2023-09-09 01:33:47 -0300

    WIP

--- resources/js/Pages/XlsUpload.vue
+++ resources/js/Pages/XlsUpload.vue
@@ -318,8 +318,8 @@
                 >XLS file</label
               >
               <input
-                class="block w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                id="small_size"
+                class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="default_size"
                 type="file"
               />
             </div>
